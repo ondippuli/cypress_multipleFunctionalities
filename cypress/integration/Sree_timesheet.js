8df8ec5bf093788e4269876d1b0f4d9a4be04f17
Saravanaprabhu Thangavel <ondi@Saravanaprabhus-MacBook-Pro.local>
2021-04-23 16:29:24 -0400

    changed git version and added new sree_timesheet.js

---
+++ cypress/integration/Sree_timesheet.js
@@ -0,0 +1,46 @@
+describe("Timesheet", function () {
+  it("Timesheet No:1", function () {
+    cy.visit(
+      "https://my.springahead.com/go/Account/LogOn?ReturnUrl=%2fgo%2fHome%2fIndex"
+    );
+
+    const company = "CAS Group";
+    const uname = "sreekala.ullatil@casgroup.com";
+    const pwd = "Ammu@0110";
+    const hours = 8;
+    const me = "ullatil";
+
+    // Enter User credentials
+    cy.get(".editor-field > #CompanyLogin").type(company).should("exist");
+    expect(company).to.be.a("string").and.not.be.empty;
+    cy.get(".editor-field > #UserName").type(uname).should("exist");
+    expect(uname).to.be.a("string").and.not.be.empty;
+    cy.get("#Password").type(pwd, { log: false }).should("exist");
+    cy.get("#submit").click();
+
+    // Selecting the project
+    cy.get(".small").click();
+    cy.get(
+      ":nth-child(1) > .wsParentTd > .wrapSelect > .wrapSelectDisplay > .wrapSelectDisplayTable > tbody > tr > .wrapSelectTexttd > .wrapSelectText"
+    ).type(me);
+    cy.get(":nth-child(3) > .ui-corner-all").click();
+
+    // Entering the hours
+    cy.get(":nth-child(1) > :nth-child(-n+7) > .hours").then((els) => {
+      [...els].forEach((el) => cy.wrap(el).type(hours));
+    });
+
+    //Validating for 40hrs
+    cy.get("input.htot.text.bold").should("have.value", 40);
+    cy.get(".htott").should("have.value", 40);
+
+    // Save the data
+    // cy.get('#btn_save_continue').click()
+
+    // Save and close the data
+    // cy.get("#btn_save");
+
+    // Submit the timesheet
+    // cy.get('#submitall').click()
+  });
+});
--- cypress/integration/practice_projects/multipleFunctionality.js
+++ cypress/integration/practice_projects/multipleFunctionality.js
@@ -1,6 +1,32 @@
 describe("Multiple functionalities", function () {
   it("Checkbox and radio button", function () {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get("#checkBoxOption1").check();
+    cy.visit("https://rahulshettyacademy.com/angularpractice/");
+    // Type text
+
+    const name = "Rose Gold";
+    const email = "abc@gmail.com";
+    const pwd = 123456;
+
+    cy.get(":nth-child(1) > .form-control")
+      .type(name)
+      .should("be.attr", "minlength", 2);
+    expect(name).to.be.a("string").and.not.be.empty;
+
+    cy.get(":nth-child(2) > .form-control")
+      .type(email)
+      .should("include.value", "@gmail.com");
+
+    //  Hiding password
+    cy.get(":nth-child(3) > .form-control").type(pwd, { log: false });
+    assert.isNumber(pwd, "val is number", { log: false });
+
+    // Checkbox and radio button
+    cy.get("#exampleCheck1").check().should("be.checked");
+    cy.get("#inlineRadio1").check({ force: true });
+
+    // select from static dropdown
+    cy.get("#exampleFormControlSelect1").select("Female");
+
+    cy.get(".ng-untouched").should("have.value", name);
   });
 });
